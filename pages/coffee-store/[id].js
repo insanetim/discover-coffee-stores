1868ea00cfd3ae4c9164cf58dae9cf78961a779a
insanetim <47120724+insanetim@users.noreply.github.com>
2024-03-14 12:45:30 +0200

    added foursquare api

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,44 +1,110 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import cls from 'classnames'
 
-import coffeeStoresData from '../../data/coffee-stores.json'
+import { fetchCoffeeStores } from '@/lib/coffee-stores'
+import styles from '@/styles/coffee-store.module.css'
 
 export async function getStaticProps({ params }) {
+  const coffeeStores = await fetchCoffeeStores()
+
   return {
     props: {
-      coffeeStore: coffeeStoresData.find(
-        coffeeStore => coffeeStore.id.toString() === params.id
+      coffeeStore: coffeeStores.find(
+        coffeeStore => coffeeStore.fsq_id.toString() === params.id
       ),
     },
   }
 }
 
 export async function getStaticPaths() {
-  const paths = coffeeStoresData.map(coffeeStore => ({
-    params: { id: coffeeStore.id.toString() },
+  const coffeeStores = await fetchCoffeeStores()
+  const paths = coffeeStores.map(coffeeStore => ({
+    params: { id: coffeeStore.fsq_id.toString() },
   }))
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
 const CoffeeStore = ({ coffeeStore }) => {
   const router = useRouter()
 
-  const { address, name, neighbourhood } = coffeeStore
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  const { address, name, neighbourhood, imgUrl } = coffeeStore
+
+  const handleUpvoteButton = () => {}
 
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href='/'>Back to home</Link>
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href='/'>Back to home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            className={styles.storeImg}
+            src={
+              imgUrl ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
+            width={600}
+            height={360}
+            alt={name}
+            priority
+          />
+        </div>
+
+        <div className={cls('glass', styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/places.svg'
+              width='24'
+              height='24'
+              alt='address icon'
+            />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/nearMe.svg'
+              width='24'
+              height='24'
+              alt='neighbourhood icon'
+            />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/star.svg'
+              width='24'
+              height='24'
+              alt='star icon'
+            />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button
+            className={styles.upvoteButton}
+            onClick={handleUpvoteButton}
+          >
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import Banner from '../components/Banner'
-import Card from '../components/Card'
-import styles from '../styles/Home.module.css'
-
-import coffeeStoresData from '../data/coffee-stores.json'
+import { fetchCoffeeStores } from '@/lib/coffee-stores'
+import Banner from '@/components/Banner'
+import Card from '@/components/Card'
+import styles from '@/styles/Home.module.css'
 
 export async function getStaticProps() {
+  const coffeeStores = await fetchCoffeeStores()
+
   return {
     props: {
-      coffeeStores: coffeeStoresData,
+      coffeeStores,
     },
   }
 }
@@ -46,10 +47,13 @@ export default function Home({ coffeeStores }) {
             <div className={styles.cardLayout}>
               {coffeeStores.map(coffeeStore => (
                 <Card
-                  key={coffeeStore.id}
+                  key={coffeeStore.fsq_id}
                   name={coffeeStore.name}
-                  imgUrl={coffeeStore.imgUrl}
-                  href={`/coffee-store/${coffeeStore.id}`}
+                  imgUrl={
+                    coffeeStore.imgUrl ||
+                    'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                  }
+                  href={`/coffee-store/${coffeeStore.fsq_id}`}
                 />
               ))}
             </div>
